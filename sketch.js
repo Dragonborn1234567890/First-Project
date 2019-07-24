fc9fb2083e804f10f05d9ea8bef9262fde0eee04
Dragonborn1234567890 <53178838+Dragonborn1234567890@users.noreply.github.com>
2019-07-24 17:16:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
-
+var  obstacle, cloud;
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadImage("trex_collided.png");
@@ -39,4 +39,42 @@ function draw() {
   
   trex.collide(invisibleGround);
   drawSprites();
+}
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(400,365,10,40);
+    obstacle.velocityX = - (6 + 3*count/100);
+    
+    //generate random obstacles
+    var rand = random(1,6);
+    obstacle.setAnimation("obstacle" + rand);
+    
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 70;
+    //add each obstacle to the group
+    ObstaclesGroup.add(obstacle);
+  }
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(400,320,40,10);
+    cloud.y = random(280,320);
+    cloud.setAnimation("cloud");
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 134;
+    
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //add each cloud to the group
+    CloudsGroup.add(cloud);
+  }
+  
 }
