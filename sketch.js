cfc0084ac9d931dac230ec3ddd15e32025a4dba1
Dragonborn1234567890 <53178838+Dragonborn1234567890@users.noreply.github.com>
2019-08-02 18:12:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,81 +1,32 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-var  obstacle, cloud;
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png")
-}
-
 function setup() {
-  createCanvas(600, 200);
-  New = createSprite(100,100,100,100);
-  New.shapecolour="red";
-  trex = createSprite(50,180,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.scale = 0.5;
-  
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -2;
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  //fixedRect.debug = true;
+  movingRect = createSprite(400, 300,80,30);
+  movingRect.shapeColor = "green";
+  //movingRect.debug = true;
+ movingRect.velocityY = -5;
+ fixedRect.velocityY = 5;
 }
+  
 
 function draw() {
-  background(200);
-  
-  if(keyDown("space")) {
-    trex.velocityY = -10;
+  background(255,255,255); 
+  /*movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  if(checkBounce(movingRect,fixedRect)){
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "blue";
+    movingRect.velocityY  = 5;
+    fixedRect.velocityY = -5;
   }
-  
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
-    ground.x = ground.width/2;
-  }
-  
-  trex.collide(invisibleGround);
+  else{
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+    movingRect.velocityY = -5;
+ fixedRect.velocityY = 5;
+  }*/
+  checkBounce(movingRect,fixedRect);
   drawSprites();
 }
-/*function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(400,365,10,40);
-    obstacle.velocityX = - (6 + 3*count/100);
-    
-    //generate random obstacles
-    var rand = random(1,6);
-    obstacle.setAnimation("obstacle" + rand);
-    
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
-    obstacle.lifetime = 70;
-    //add each obstacle to the group
-    ObstaclesGroup.add(obstacle);
-  }
-}
-
-function spawnClouds() {
-  //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
-    var cloud = createSprite(400,320,40,10);
-    cloud.y = random(280,320);
-    cloud.setAnimation("cloud");
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
-    
-     //assign lifetime to the variable
-    cloud.lifetime = 134;
-    
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth + 1;
-    
-    //add each cloud to the group
-    CloudsGroup.add(cloud);
-  }
-  
-}*/
